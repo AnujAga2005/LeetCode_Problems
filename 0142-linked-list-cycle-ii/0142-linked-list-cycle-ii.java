44bc24e982c8db402776f14e42c2b79856b6a574
Anuj Agarwal <anujagarwal1704@gmail.com>
2025-09-13 16:35:37 +0530

    Time: 0 ms (100%), Space: 44.6 MB (51.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -20,12 +20,12 @@ public class Solution {
             slow = slow.next;
             fast = fast.next.next;
             if(slow == fast){
-                ListNode ptr = head;
-                while(ptr!=slow){
-                    ptr=ptr.next;
+                slow = head;
+                while(slow!=fast){
+                    fast=fast.next;
                     slow = slow.next;
                 }
-                return ptr;
+                return slow;
             }
         }
         return null;
