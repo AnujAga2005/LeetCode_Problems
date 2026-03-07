e1e9f5c534035c65359c08a89a3f588a12ee6069
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-03-07 10:06:29 +0530

    Time: 1 ms (99.9%), Space: 44 MB (97.66%) - LeetHub

--- 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.java
@@ -1,16 +1,15 @@
 class Solution {
-    public static int recurse(String s, int i){
-        if(i<0 || (s.charAt(i)!='a' && s.charAt(i)!='e' && s.charAt(i)!='i' && s.charAt(i)!='o' && s.charAt(i)!='u')){
-            return i;
-        }
-        i--;
-        return recurse(s, i);
-    }
     public String trimTrailingVowels(String s) {
         int i = s.length()-1;
-        int sub = recurse(s,i);
-        String ans = s.substring(0,sub+1);
-        return ans;
+        while(i>=0){
+            char c = s.charAt(i);
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+                i--;
+            }else{
+                break;
+            }
+        }
+        return s.substring(0,i+1);
         
     }
 }
