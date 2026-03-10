c7ac2203a5587f0456cdc67a92e69ccccf761874
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-03-10 09:21:27 +0530

    Time: 5 ms (44.64%), Space: 45.1 MB (50.52%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,28 +1,25 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        long v1 = Long.MIN_VALUE;
-        long v2 = Long.MIN_VALUE;
-        long v3 = Long.MIN_VALUE;
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        Set<Integer> set = new HashSet();
         for(int i=0; i<nums.length; i++){
-            if( nums[i]==v1 || nums[i]==v2 || nums[i]==v3 ){
+            if(set.contains(nums[i])){
                 continue;
             }
-            if(nums[i]>v1){
-                v3=v2;
-                v2=v1;
-                v1=nums[i];
-            }
-            else if(nums[i]>v2){
-                v3=v2;
-                v2=nums[i];
-            }
-            else if(nums[i]>v3){
-                v3=nums[i];
+            if(pq.size()<3){
+                pq.add(nums[i]);
+                set.add(nums[i]);
+            } 
+            else{
+                if(nums[i]>pq.peek()){
+                    set.remove(pq.poll());
+                    pq.add(nums[i]);
+                    set.add(nums[i]);
+                }
             }
         }
-        if(v3==Long.MIN_VALUE) return (int)v1;
-        return (int)v3;
-        
+        if(pq.size()!=2) return pq.peek();
+        pq.poll();
+        return pq.peek();
     }
 }
