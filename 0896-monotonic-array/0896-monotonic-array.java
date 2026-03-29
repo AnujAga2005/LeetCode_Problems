5316f4b6ddcef144f6f40d06c148e17aaeb2241e
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-03-29 15:12:12 +0530

    Time: 2 ms (67.79%), Space: 85.1 MB (73.21%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -6,15 +6,12 @@ class Solution {
         for(int i=0; i<nums.length-1; i++){
             if(nums[i]>nums[i+1]){
                 monoInc = false;
-                break;
             }
-        }
-        for(int i=0; i<nums.length-1; i++){
             if(nums[i]<nums[i+1]){
                 monoDec = false;
-                break;
             }
         }
+        
         return (monoInc || monoDec);
     }
 }
