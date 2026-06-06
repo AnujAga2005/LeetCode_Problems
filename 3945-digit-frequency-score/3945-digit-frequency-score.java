55e0c99d11d1d4e8d1bf5844cedce232489c66b3
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-06-06 20:55:54 +0530

    Time: 1 ms (99.33%), Space: 42.7 MB (47.4%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -3,17 +3,13 @@ class Solution {
         if(n==0){
             return 0;
         }
-        HashMap<Integer,Integer> hm = new HashMap<>();
         int ans = 0;
         while(n>0){
             int temp = n%10;
-            hm.put(temp, hm.getOrDefault(temp,0)+1);
+            ans+=temp;
             n=n/10;
         }
-        for(int key: hm.keySet()){
-            int val = key*hm.get(key);
-            ans +=val;
-        }
+        
         return ans;
     }
 }
