beefbd455bed8ac4f84546bb363a19f4880ec7b2
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-06-06 09:58:12 +0530

    Time: 1 ms (99.97%), Space: 47.1 MB (27.1%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,19 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length<2){
-            return nums[0];
+        int ans = 0;
+        for(int num: nums){
+            ans^=num;
         }
-        Arrays.sort(nums);
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        for(int i=1; i<nums.length-1; i++){
-            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]){
-                return nums[i];
-            }
-        }
-        return nums[nums.length-1];
-
+        return ans;
         
     }
 }
