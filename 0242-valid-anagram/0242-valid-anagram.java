5ca5ce03fe0c7ffe03975b517b96313601b0c72b
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-06-06 08:26:48 +0530

    Time: 20 ms (9.65%), Space: 46.6 MB (23.49%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int j=0; j<t.length(); j++){
+            if(hm.containsKey(t.charAt(j))){
+                if(hm.get(t.charAt(j))==1){
+                    hm.remove(t.charAt(j));
+                }else{
+                    hm.put(t.charAt(j),hm.get(t.charAt(j))-1);
+                }
+            }else{
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
