421c31035970e04134a89be6261b5f798b3aa341
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-06-09 21:01:12 +0530

    Time: 0 ms (100%), Space: 42 MB (71.21%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int cw(int n, int[] dp){
+        dp[0]= 1;
+        for(int i=1; i<=n; i++){
+            if(i==1){
+                dp[i] = 1;
+            }else{
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        }
+        return dp[n];
+        
+    }
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        int ways = cw(n,dp);
+        return ways;
+
+        
+    }
+}
