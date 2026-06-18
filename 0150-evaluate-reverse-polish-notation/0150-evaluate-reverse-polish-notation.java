1375151cb2de6b4e7bcfb999eba9422ae83a7a41
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-06-18 20:38:21 +0530

    Time: 5 ms (96.87%), Space: 45.5 MB (16.3%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,36 +1,28 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        if(tokens.length==1){
-            return Integer.parseInt(tokens[0]);
-        }
         Stack<Integer> s = new Stack<>();
-        int a,b;
-        for(int i=0; i<tokens.length; i++){
-            String t = tokens[i];
-            if(t.equals("+")){
-                s.push(s.pop()+s.pop());
-                continue;
+        int first = 0;
+        int second = 0;
+        for(String token: tokens){
+            switch(token){
+                
+                case "+": s.push( s.pop() + s.pop()); break;
+                
+                case "-":
+                    second = s.pop();
+                    first = s.pop();
+                    s.push( first - second); break;
+                
+                case "*": s.push( s.pop() * s.pop()); break;
+               
+               case "/":
+                    second = s.pop();
+                    first = s.pop();
+                    s.push( first / second); break;
+                default: s.push(Integer.parseInt(token));
             }
-            else if(t.equals("-")){
-                a = s.pop();
-                b = s.pop();
-                s.push(b-a);
-                continue;
             }
-            else if(t.equals("*")){
-                s.push(s.pop()*s.pop());
-                continue;
-            }
-            else if(t.equals("/")){
-                a = s.pop();
-                b = s.pop();
-                s.push(b/a);
-                continue;
-            }
-            s.push(Integer.parseInt(tokens[i]));
-            
+            return s.peek();
         }
-        return s.pop();
-        
     }
-}
+
