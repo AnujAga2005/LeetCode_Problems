99102dcf043c8d3e3bfea701704ff339abdf563a
Anuj Agarwal <anujagarwal1704@gmail.com>
2026-08-01 10:12:11 +0530

    Time: 0 ms (100%), Space: 43.4 MB (62.38%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,20 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] =0;
+        }
         int[] ans = new int[digits.length+1];
-        int j = ans.length-1;
-        for(int i=1; i<=digits.length; i++){
-            ans[i] = digits[i-1];
-        }
-        ans[j]+=1;
-        while(ans[j--]>=10){
-            ans[j+1] = ans[j+1]-10;
-            ans[j]  = ans[j]+1;
-        }
-        if (ans[0] == 0) {
-            int[] res = new int[digits.length];
-            System.arraycopy(ans, 1, res, 0, digits.length);
-            return res;
-        }
+        ans[0] = 1;
         return ans;
+        
+        
     }
 }
